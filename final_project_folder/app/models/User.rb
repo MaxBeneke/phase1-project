082e85fb5e2428c39d973889afa8365f3f444002
Kwaku <kwakugyapong219@gmail.com>
2021-01-25 17:53:35 -0500

    added sign up

--- final_project_folder/app/models/User.rb
+++ final_project_folder/app/models/User.rb
@@ -2,4 +2,11 @@ class User < ActiveRecord::Base
 
     has_many :reservations
     has_many :courts, through: :reservations
+
+    def self.sign_up(username, password, email)
+        User.create(username: username, password: password, email: email)
+        
+    end
+
+
 end
